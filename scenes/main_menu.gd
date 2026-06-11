bcbc82b9e544c34b29af31ab162a54ffcdccda80
SrslyKail <90731959+SrslyKail@users.noreply.github.com>
2026-06-11 14:51:51 -0700

    Expose scene variables in editor :D

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,12 +1,16 @@
 extends Control
+@export var NewGameButton: Button
+@export var ContinueButton: Button
+@export var SettingsButton: Button
+@export var QuitButton: Button
+@export var SettingsScene: PackedScene
 
 # register event listeners 
 func _ready() -> void:
-	$CenterContainer/VBoxContainer/NewGameButton.pressed.connect(_on_new_game_pressed)
-	$CenterContainer/VBoxContainer/ContinueButton.pressed.connect(_on_continue_pressed)
-	$CenterContainer/VBoxContainer/SettingsButton.pressed.connect(_on_settings_pressed)
-	$CenterContainer/VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
-	
+	NewGameButton.pressed.connect(_on_new_game_pressed)
+	ContinueButton.pressed.connect(_on_continue_pressed)
+	SettingsButton.pressed.connect(_on_settings_pressed)
+	QuitButton.pressed.connect(_on_quit_pressed)
 
 func _on_new_game_pressed() -> void:
 	print("the gizzy is fresh")
@@ -15,7 +19,7 @@ func _on_continue_pressed() -> void:
 	print("contibule")
 	
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+	get_tree().change_scene_to_packed(SettingsScene)
 	print("netting")
 
 func _on_quit_pressed() -> void:
