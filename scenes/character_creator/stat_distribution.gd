bdaa43faeb7ccf0ce1fb96429b70918560c76936
brysonomicon <bslindy@gmail.com>
2026-06-12 16:30:34 -0700

    added character creator scene with sub scenes for each task.

---
+++ scenes/character_creator/stat_distribution.gd
@@ -0,0 +1,7 @@
+extends Control
+
+@export var title: String = "Stat Distribution"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -4,6 +4,7 @@ extends Control
 @export var SettingsButton: Button
 @export var QuitButton: Button
 @export var SettingsScene: PackedScene
+@export var CharCreateScene: PackedScene
 
 func _ready() -> void:
 	NewGameButton.pressed.connect(_on_new_game_pressed)
@@ -12,6 +13,7 @@ func _ready() -> void:
 	QuitButton.pressed.connect(_on_quit_pressed)
 
 func _on_new_game_pressed() -> void:
+	SceneRouter.goto(CharCreateScene)
 	print("new game pressed")
 	
 func _on_continue_pressed() -> void:
@@ -22,4 +24,5 @@ func _on_settings_pressed() -> void:
 	print("settings pressed")
 
 func _on_quit_pressed() -> void:
+	print("quit pressed")
 	get_tree().quit()
