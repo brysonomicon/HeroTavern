20794681524fe0232dae05f63d1611c01084f6b8
SrslyKail <90731959+SrslyKail@users.noreply.github.com>
2026-06-19 19:18:06 -0700

    finish import script

--- scripts/Editor/CreateCharacterParts.gd
+++ scripts/Editor/CreateCharacterParts.gd
@@ -28,23 +28,14 @@ func iterate_folder(folderPath: String) -> void:
 			var nextPath: String = baseDir.get_current_dir().path_join(current)
 			iterate_folder(nextPath)
 		elif current.get_extension() != "png":
-			continue
+			pass
+		elif current == "characterBase.png":
+			pass
 		else:
 			process_sprite(folderPath, current)
 		current = baseDir.get_next()
 
 func process_sprite(spriteFolder: String, spriteName: String) -> void:
-	print("Processing sprite folder: ", spriteFolder, spriteName)
-	# Guard clauses
-	if spriteName.ends_with(".import"):
-#		print("Ignoring imports.")
-		return
-	if spriteName == "icon.svg":
-#		print("Ignoring game icon.")
-		return
-	if spriteName == "characterBase.png":
-#		print("Ignoring character base art.")
-		return
 	if spriteFolder.ends_with("default"):
 		return
 	
@@ -63,7 +54,8 @@ func process_sprite(spriteFolder: String, spriteName: String) -> void:
 		if spriteNameClean.get_basename() == file.get_basename():
 			push_warning("Found part for ", file)
 			return
-	create_character_part(partDir, spriteDirPath, spriteName)
+	
+	create_character_part(partDir.get_current_dir(), spriteFolder, spriteName)
 			
 func create_character_part_folder(spriteFolder: String) -> String:
 	var partFolderName: String = spriteFolder.rsplit("/", true, 1)[1]
@@ -85,10 +77,7 @@ func create_character_part_folder(spriteFolder: String) -> String:
 	
 	return newPartPath
 
-func create_character_part(partDir: DirAccess, spriteFolder: String, spriteFile: String) -> void:
-	var newPart: CharacterPart = CharacterPart.new()
-
-	newPart.displayName = spriteFile.to_camel_case().get_basename()
+func create_character_part(partDir: String, spriteFolder: String, spriteFile: String) -> void:
 	var spritePath: String = spriteFolder.path_join(spriteFile)
 
 	var file: FileAccess = FileAccess.open(spritePath, FileAccess.READ)
@@ -101,6 +90,16 @@ func create_character_part(partDir: DirAccess, spriteFolder: String, spriteFile:
 	var error: Error = image.load_png_from_buffer(buffer)
 	if error != OK:
 		push_error("Failed to load %s from buffer!" % spritePath)
-		
+	
+	var newPart: CharacterPart = CharacterPart.new()
+	newPart.displayName = spriteFile.to_camel_case().get_basename()
 	newPart.texture = ImageTexture.create_from_image(image)
 	
+	var partName: String = newPart.displayName + ".tres"
+	var resourcePath = partDir.path_join(partName)
+	var save_error: Error = ResourceSaver.save(newPart, resourcePath)
+	if save_error != OK:
+		push_error("Failed to save ", partName)
+	else:
+		print("Created new CharacterPart: ", newPart.displayName)
+		EditorInterface.get_resource_filesystem().scan()
