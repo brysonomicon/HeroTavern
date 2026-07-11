afdfcae897a146aa1fbc4c476f8d8d4c56c7693e
brysonomicon <bslindy@gmail.com>
2026-07-10 21:02:08 -0700

    added comment explaining character cretor wizard logic in creator_main

--- scenes/character_creator/creator_main.gd
+++ scenes/character_creator/creator_main.gd
@@ -1,3 +1,12 @@
+## control script for the character creation wizard when the wizard is open,
+## all steps are created with only one being made visible at a time.
+## each step holds member data for the character in private variables. once each
+## step is confirmed complete, the data in the different steps is applied to the
+## finished character object.
+
+## steps could be abstracted into a specific control that formalizes the contract
+## of assign_property
+
 extends Control
 
 signal character_created(character)
