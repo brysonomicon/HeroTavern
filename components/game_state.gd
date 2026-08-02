8c50c2975f6f58f2b218a2e73004c996c754f853
brysonomicon <bslindy@gmail.com>
2026-08-01 21:03:19 -0700

    forgot to save scene

--- components/game_state.gd
+++ components/game_state.gd
@@ -1,11 +1,9 @@
-extends Node
+class_name GameState
+extends Resource
 
+@export var tavern_manager: Character
+@export var hero_roster: HeroRoster
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _init() -> void:
+	if hero_roster == null:
+		hero_roster = HeroRoster.new()
